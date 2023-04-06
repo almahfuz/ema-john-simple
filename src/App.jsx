90b441823012254c1d4447504cd433fb1b1e5d78
almahfuz <abujafaralmahfuz@gmail.com>
2023-04-06 21:51:46 +0100

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Shops from "./components/Shops/Shops";
 
 
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-
       <Header></Header>
+      <Shops></Shops>
 
     </div>
 
