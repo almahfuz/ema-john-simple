42297b7d6141c5cad691f68ceab679a398a87bab
almahfuz <abujafaralmahfuz@gmail.com>
2023-04-08 02:29:33 +0100

    modified the localstorage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,12 +3,17 @@ import "./Cart.css";
 
 const Cart = ({ cart }) => {
   // const cart =props.cart;
-  console.log(cart);
+//   console.log(cart);
   let totalPrice = 0;
-  let TotalShipping =0;
+  let TotalShipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    totalPrice = totalPrice + product.price;
+
+    //product.quantity = product.quantity || 1;
+
+    totalPrice = totalPrice + product.price * product.quantity;
     TotalShipping = TotalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
 //   this is tax section, tax is 7% 
   const tax =totalPrice*7/100;
@@ -19,7 +24,7 @@ const Cart = ({ cart }) => {
     <div>
       <div className="cart">
         <h4>Order Summary </h4>
-        <p>Selected items: {cart.length} </p>
+        <p>Selected items: {quantity} </p>
         <p>Total Price : ${totalPrice} </p>
         <p>Total Shipping : ${TotalShipping} </p>
         <p>Tax : ${tax.toFixed(2)} </p>
